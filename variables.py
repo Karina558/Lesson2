a34050069c54183ce57aa033ed57a7a8d5230fbe
Karina <kidiaievakarina@gmail.com>
2023-12-16 21:26:17 +0200

    done dz/ex-3

--- variables.py
+++ variables.py
@@ -1,11 +1,7 @@
 try:
-    salary = float(input('Enter your salary: '))
-    loan = float(input('Enter your credit payment: '))
-    utilities = float(input('Enter your debt for utilities: '))
-    result = salary - (loan + utilities)
-    if result > 0:
-        print('\t\033[1;34mYour net income: ', result, '\u20B4\033[0m')
-    elif result < 0:
-        print('\t\033[91mYour losess: ', result, '\u20B4\033[0m')
+    diagonal_1 = float(input('Enter diagonal 1 of rhombus: '))
+    diagonal_2 = float(input('Enter diagonal 1 of rhombus: '))
+    result = (diagonal_1*diagonal_2)/2
+    print('Square of rhombus is: ', result)
 except Exception as e:
     print (e)
