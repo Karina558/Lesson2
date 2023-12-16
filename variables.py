bda95a353a9653b6cbefdcfdb1ec058242b970a2
Karina <kidiaievakarina@gmail.com>
2023-12-16 18:08:54 +0200

    ex 2 done

--- variables.py
+++ variables.py
@@ -1,9 +1,7 @@
 try:
-    number_1 = int(input('Enter number 1: '))
-    number_2 = int(input('Enter number 2: '))
-    print('Sum:', number_1 + number_2)
-    print('Subtraction:', number_1 - number_2)
-    print('Multiplications:', number_1 * number_2)
-    print('Division:', number_1 / number_2)
+    number = int(input('Enter number: '))
+    percentage = int(input('Enter percentage: '))
+    result = (number * percentage) / 100
+    print (result)
 except Exception as e:
     print (e)
