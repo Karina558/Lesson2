e650ce20d7ff7c0b2da8d61b18278d1c9f54904e
Karina <kidiaievakarina@gmail.com>
2023-12-16 20:53:49 +0200

    done dz/ex-2

--- variables.py
+++ variables.py
@@ -1,18 +1,11 @@
 try:
-    number_1 = float(input('Enter number 1: '))
-    number_2 = float(input('Enter number 2: '))
-    number_3 = float(input('Enter number 3: '))
-    x = str(input('Enter operation (\'+\' or \'*\' or \'all\' to see Sum and Multiplications): '))
-    result_1 = number_1 + number_2 + number_3
-    result_2 = number_1 * number_2 * number_3
-    if x == '+':
-        print('\033[1;34m\tSum:\033[0m ', result_1)
-    elif x == '*':
-        print('\033[1;34m\tMultiplications:\033[0m ', result_2)
-    elif x == 'all':
-        print('\033[1;34m\tSum:\033[0m ', result_1)
-        print('\033[1;34m\tMultiplications:\033[0m ', result_2)
-    else:
-        print('\033[91m\tError!\033[0m Please, enter \'+\' to Sum or \'*\' to Multiplicate.')
+    salary = float(input('Enter your salary: '))
+    loan = float(input('Enter your credit payment: '))
+    utilities = float(input('Enter your debt for utilities: '))
+    result = salary - (loan + utilities)
+    if result > 0:
+        print('\t\033[1;34mYour net income: ', result, '\u20B4\033[0m')
+    elif result < 0:
+        print('\t\033[91mYour losess: ', result, '\u20B4\033[0m')
 except Exception as e:
     print (e)
