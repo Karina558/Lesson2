7f77eba3ccb15082d8c5218e3db25fd0fd184823
Karina <kidiaievakarina@gmail.com>
2023-12-16 20:16:28 +0200

    done

--- variables.py
+++ variables.py
@@ -1,7 +1,18 @@
 try:
-    number = float(input('Enter number: '))
-    percentage = float(input('Enter percentage: '))
-    result = (number * percentage) / 100
-    print ('\n\tPersentage of',number,'=', result,'%')
+    number_1 = float(input('Enter number 1: '))
+    number_2 = float(input('Enter number 2: '))
+    number_3 = float(input('Enter number 3: '))
+    x = str(input('Enter operation (\'+\' or \'*\' or \'all\' to see Sum and Multiplications): '))
+    result_1 = number_1 + number_2 + number_3
+    result_2 = number_1 * number_2 * number_3
+    if x == '+':
+        print('\033[1;34m\tSum:\033[0m ', result_1)
+    elif x == '*':
+        print('\033[1;34m\tMultiplications:\033[0m ', result_2)
+    elif x == 'all':
+        print('\033[1;34m\tSum:\033[0m ', result_1)
+        print('\033[1;34m\tMultiplications:\033[0m ', result_2)
+    else:
+        print('\033[91m\tError!\033[0m Please, enter \'+\' to Sum or \'*\' to Multiplicate.')
 except Exception as e:
     print (e)
