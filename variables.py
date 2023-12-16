89749ac94fe9457421addf6a73fea37bd318a99e
Karina <kidiaievakarina@gmail.com>
2023-12-16 20:28:49 +0200

    done lab/ ex-3

--- variables.py
+++ variables.py
@@ -1,7 +1,6 @@
 try:
     width = float(input('Enter width: '))
     height = float(input('Enter height: '))
-
     result = width + height
     print('\tSquare is: ', result)
 except Exception as e:
